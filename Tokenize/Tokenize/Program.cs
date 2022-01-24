bd4b0b18159ac40c2b87779a621653a5ab7a3d46
Austin Johnson <austin.johnson@motoman.com>
2022-01-24 10:24:00 -0500

    Initial Push

---
+++ Tokenize/Tokenize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tokenize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
