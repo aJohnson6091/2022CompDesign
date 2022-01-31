bc02c8f425f4678776fd2b888fd0d92ebfbc8df2
Austin Johnson <austin.johnson@motoman.com>
2022-01-31 16:52:51 -0500

    Start of Shunting lab

---
+++ ShuntingYard/ShuntingYard/Functions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuntingYard
+{
+    class Functions
+    {
+        public static int precedence(string sym)
+        {
+            if (sym.Equals("POWOP"))
+                return 50;
+            if (sym.Equals("MULOP"))
+                return 20;
+            if (sym.Equals("ADDOP"))
+                return 10;
+            return 0;
+        }
+
+        private static char associativity(string sym)
+        {
+            if (sym.Equals("POWOP"))
+                return 'R';
+            return 'L';
+        }
+
+        public static void handleOperator(Token t, Stack<TreeNode> operatorStack, Stack<TreeNode> nodeStack)
+        {
+            char assoc = associativity(t.sym);
+            while (true)
+            {
+                if (operatorStack.Count == 0)
+                    break;
+                string A = operatorStack.Peek().sym;
+                if ((assoc == 'L' && precedence(A) >= precedence(t.sym)) || (assoc == 'R' && precedence(A) > precedence(t.sym)))
+                {
+                    TreeNode opNode = operatorStack.Pop();
+                    TreeNode c1 = nodeStack.Pop();
+                    TreeNode c2 = nodeStack.Pop();
+                    opNode.children.Add(c2);
+                    opNode.children.Add(c1);
+                    nodeStack.Push(opNode);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            operatorStack.Push(new TreeNode(t.sym, t));
+        }
+
+        
+    }
+}
---
+++ ShuntingYard/ShuntingYard/ShuntingYard.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShuntingYard
+{
+    class ShuntingYard
+    {
+        static void Main(string[] args)
+        {
+            string gfile = "Gram.txt";
+            string gramspec;
+            using (var r = new System.IO.StreamReader(gfile))
+            {
+                gramspec = r.ReadToEnd();
+            }
+            var g = new Grammar(gramspec);
+            var tokenizer = new Tokenizer(g);
+            string input;
+
+            //get input
+            input = Console.ReadLine();
+
+            tokenizer.setInput(input);
+            List<Token> tokens = new List<Token>();
+            Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+            Stack<TreeNode> operatorStack = new Stack<TreeNode>();
+            while (true)
+            {
+                try
+                {
+                    Token t = tokenizer.next();
+                    
+                    if (t.sym == "$")
+                    {
+                        break;
+                    }
+
+                    if(t.lexeme == "-")
+                    {
+                        string p = tokens[tokens.Count - 1].sym;
+                        if(p == null || p.Equals("LP") || p.Equals("MULOP") || p.Equals("POWOP")|| p.Equals("ADDOP"))
+                        {
+                            t.sym = "NEGATE";
+                        }
+                    }
+
+                    tokens.Add(t);
+
+                    if (t.sym == "NUM" || t.sym == "ID")
+                    {
+                        nodeStack.Push(new TreeNode(t.sym, t));
+                    }
+                    else
+                    {
+                        Functions.handleOperator(t, operatorStack, nodeStack);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    break;
+
+                }
+            }while(operatorStack.Count != 0)
+            {
+                TreeNode opNode = operatorStack.Pop();
+                TreeNode c1 = nodeStack.Pop();
+                TreeNode c2 = nodeStack.Pop();
+                opNode.children.Add(c2);
+                opNode.children.Add(c1);
+                nodeStack.Push(opNode);
+            }
+
+        }
+
+        
+    }
+}
